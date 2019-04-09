dfe9a6dcb7b7812907d92d3a22a4261da5d5736c
Leo Qvarnström <leo.w.qvarnstrom@gmail.com>
2019-04-09 13:54:18 +0300

    Käyttäjätilien hallinta

---
+++ MokkiApp/MokkiApp/FrmLoginTEMP.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MokkiApp {
+    public partial class FrmLoginTEMP : Form {
+        public FrmLoginTEMP() {
+            InitializeComponent();
+        }
+
+        //tbPassword kohdassa näppäimen painaisu
+        private void tbPassword_KeyPress(object sender, KeyPressEventArgs e) {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter)) {
+                btnOk_Click(sender, e);
+            }
+        }
+
+        //OK-napin click
+        private void btnOk_Click(object sender, EventArgs e) {
+            try {
+                if (UserUtils.LegitPassword(tbPassword.Text) && UserUtils.UserFound(tbUsername.Text, true)) {
+                    UserUtils.CreateUser(tbUsername.Text, tbPassword.Text, cbAdmin.Checked);
+                    lblHash.Text = UserUtils.LoggedUser.Hash;
+                    lblSalt.Text = UserUtils.LoggedUser.Salt;
+                }
+            }
+            catch {
+                UserUtils.AddErrorMessage("Käyttäjän luominen epäonnistui.");
+            }
+            //Printtaa aina kaikki virheviestit (saa muuttaa)
+            lblError.Text = UserUtils.PrintErrorMessages();
+        }
+    }
+}
